5cd03aca6765755220252ee607b4e79d1fe0eaab
Shubham Thakur <thakurshubh1999@gmail.com>
2022-11-30 15:43:57 +0530

    Taking input

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-var username = prompt("What is Your name");
-alert(" How are you "+ username);
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+function clickHandler()
+{
+    console.log("Clicked")
+    console.log("input",txtInput.value)
+}
+btnTranslate.addEventListener("click",clickHandler);
+//txtInput.addEventListener()

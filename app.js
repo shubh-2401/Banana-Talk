412f414397ded13771f8c39b508c6f1111045212
Shubham Thakur <thakurshubh1999@gmail.com>
2022-11-30 16:06:20 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+
 function clickHandler()
 {
-    console.log("Clicked")
-    console.log("input",txtInput.value)
-}
+    outputDiv.innerText = "asddff "+ txtInput.value ;
+};
 btnTranslate.addEventListener("click",clickHandler);
-//txtInput.addEventListener()
+

fad9b2fa29264f05b92aa6601a8913ad5ffa42cc
Shubham Thakur <thakurshubh1999@gmail.com>
2022-11-30 17:22:09 +0530

    Functionality done

--- app.js
+++ app.js
@@ -3,9 +3,33 @@ var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
 
+// URL of the server where our translation is done.
+var serverURL = "https://api.funtranslations.com/translate/minion.json?";
+
+
+// Function which creates a URL including our input text using the serverURL
+function getTranslationURL(input)
+{
+    return serverURL +"text="+ input;
+}
+
+function errorHandling()
+{
+    console.log("error occured",error)
+    alert("Something is wrong with the server ! Please try again later.")
+}
 function clickHandler()
 {
-    outputDiv.innerText = "asddff "+ txtInput.value ;
+    var inputText=txtInput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response =>response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;        // output
+    })
+    .catch(errorHandling)
 };
 btnTranslate.addEventListener("click",clickHandler);
 
